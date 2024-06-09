cfa606170f081f2ef1f13e45e6bad10c7fc092b0
Maximilian Schöll <max@schoell.co>
2024-06-09 12:18:37 +0200

    fix: handle SIGINT for stopping container

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+handle_sigint() {
+  echo "Received SIGINT, stopping cron..."
+  service cron stop
+  exit 0
+}
+
 printf  "${CRON_SCHEDULE} cd /app/validator && /usr/local/bin/python main.py > /proc/1/fd/1 2>/proc/1/fd/2\n" > /etc/cron.d/cronjob
 
 chmod 0644 /etc/cron.d/cronjob
@@ -7,4 +13,9 @@ chmod 0644 /etc/cron.d/cronjob
 
 printenv > /etc/environment
 
-cron -f 
+trap 'handle_sigint' SIGINT
+
+cron -f &
+CRON_PID=$!
+
+wait $CRON_PID
