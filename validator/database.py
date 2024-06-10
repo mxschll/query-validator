bd8c0a6a2efa241fd19125539168062366bc3064
Maximilian Schöll <max@schoell.co>
2024-06-10 14:46:40 +0200

    fix:typo

--- validator/database.py
+++ validator/database.py
@@ -2,7 +2,7 @@ import sqlalchemy
 
 
 def create_db_engine(uri):
-    return sqlalchemy.create_engine(uri, client_encoding='uft8')
+    return sqlalchemy.create_engine(uri, client_encoding='utf8')
 
 
 def execute_query(connection, query, params=None):
